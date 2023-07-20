5a5caf37e941c364b95d4fb14794167f9bacb833
Soe Phyu <soephyuphyuhtun99@gmail.com>
2023-07-20 09:47:46 +0630

    import team member

--- src/components/Teams/Team.tsx
+++ src/components/Teams/Team.tsx
@@ -1,27 +1,25 @@
 import "./Team.css";
-import user from "../../assets/images/user.png";
+import { members } from "./members";
 const Team = () => {
-	let arr = [0, 1, 2, 3, 4, 5];
 	return (
-		<section id="team" className="container team text-center">
+		<section id="team" className="container bg-primary team text-center">
 			<div className="section-header">
-				<p className="section-header-content color-accent">
+				<p className="section-header-content color-secondary">
 					Unleashing Team Excellence.
 				</p>
 				<header className="section-header-title">Talent Network</header>
 			</div>
 			<div className="d-flex flex-wrap space-between cards">
-				{arr.map((a, index) => (
-					<div key={a} className="card m-1 bg-secondary radius">
-						<img src={user} alt="" className="card-img" />
+				{members.map((member, index) => (
+					<div key={index} className="card m-1 bg-secondary radius">
+						<img src={member.img} alt="" className="card-img" />
 						<div className={"card-description p-1 text-left"}>
 							<header className="font-sm">
-								{index === 0 ? "Founder" : "Software Developer"}
+								{member.position}
 							</header>
-							<p className="color-accent">John Smith</p>
+							<p className="color-accent">{member.name}</p>
 							<p className="member-description mt-1 font-sm">
-								Lorem ipsum dolor sit amet consectetur
-								adipisicing elit. Quidem, minima?
+								{member.description}
 							</p>
 						</div>
 					</div>
---
+++ src/components/Teams/members.ts
@@ -0,0 +1,48 @@
+import founder from "../../assets/images/founder.jpg";
+import thihan from "../../assets/images/thihan.png";
+import dummyUser from "../../assets/images/user.png";
+
+export const members = [
+	{
+		name: "Mohan Wagle",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, minima?",
+		position: "Founder",
+		img: founder,
+	},
+	{
+		name: "Gangadar Wagle",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, minima?",
+		position: "Software Developer",
+		img: dummyUser,
+	},
+	{
+		name: "Chaw Be Lar",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, minima?",
+		position: "Software Developer",
+		img: dummyUser,
+	},
+	{
+		name: "Thihan Win Htet",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, minima?",
+		position: "Software Developer",
+		img: thihan,
+	},
+	{
+		name: "Aung Phyo Thant",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, minima?",
+		position: "Software Developer",
+		img: dummyUser,
+	},
+	{
+		name: "Soe Phyu Phyu Htun",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, minima?",
+		position: "Software Developer",
+		img: dummyUser,
+	},
+];
